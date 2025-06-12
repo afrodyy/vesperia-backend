5e1d882959a3ffe992f1e0aa9b5bcf09f89b6ed7
Herdian Afrody <herdianafrody@JTA-INFRASs-MacBook-Pro-3.local>
2025-06-12 10:16:18 +0700

    Feat: Get Form by Id

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -18,6 +18,42 @@ class FormController extends Controller
         ], 200);
     }
 
+    public function show(Form $form)
+    {
+        $form->load([
+            'fields.options' => fn($query) => $query->orderBy('id')
+        ]);
+
+        $formStructure = [
+            'id' => $form->id,
+            'name' => $form->name,
+            'fields' => $form->fields->map(function ($field) {
+                return [
+                    'id' => $field->id,
+                    'label' => $field->label,
+                    'type' => $field->type,
+                    'sub_type' => $field->sub_type,
+                    'description' => $field->description,
+                    'parent_id' => $field->parent_id,
+                    'options' => $field->options->map(function ($option) {
+                        return [
+                            'id' => $option->id,
+                            'label' => $option->label,
+                            'value' => $option->value,
+                            'parent_id' => $option->parent_id
+                        ];
+                    }),
+                ];
+            })
+        ];
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Form fetched successfully',
+            'data' => $formStructure
+        ], 200);
+    }
+
     public function store(Request $request)
     {
         $request->validate([
--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\FormFieldController;
 use App\Http\Controllers\FormFieldOptionController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/forms/{form}', [FormController::class, 'show']);
 Route::post('/forms', [FormController::class, 'store']);
 Route::post('/forms/{form}/fields', [FormFieldController::class, 'store']);
 Route::post('/fields/{field}/options', [FormFieldOptionController::class, 'store']);
